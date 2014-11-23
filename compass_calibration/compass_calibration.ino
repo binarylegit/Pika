6542969505b6a0b348be4e5f128a05b2a061b040
Daniel Fowler <binarylegit@gmail.com>
2014-11-23 12:20:53 -0700

    add updated calibration program and new calibration measurements.

--- compass_calibration/compass_calibration.ino
+++ compass_calibration/compass_calibration.ino
@@ -45,9 +45,13 @@ int baudrate = 9600;
 int compassReadings = 0;
 char compassReport[80];
 
+int operatingEnvironment = 0;
+boolean isCalibrationCompleted = false;
+
 void setup() {
 
   setupMotors();
+  setAllStop();
 
   setupSerial();
 
@@ -59,8 +63,12 @@ void setup() {
 
   analogReference(DEFAULT);
 
+  operatingEnvironment = 0;
+  compassReadings = 0;
+
   // delay 10 seconds to allow serial monitor to open
   delay(10000);
+  setMotorOperatingEnvironment();
 
 }
 
@@ -90,52 +98,125 @@ void setupMotors() {
   pinMode(leftDirectionPin, OUTPUT);
   pinMode(rightSpeedPWMPin, OUTPUT);
   pinMode(leftSpeedPWMPin, OUTPUT);
-
-  // OPERATING ENVIRONMENTS -- only activate one -----------------------------------------------------------------
-
-  // Right motor forward 50, Left motor reverse 50
-//  digitalWrite(rightDirectionPin, FORWARD);
-//  digitalWrite(leftDirectionPin, REVERSE);
-//  analogWrite(rightSpeedPWMPin, 50);
-//  analogWrite(leftSpeedPWMPin, 50);
-
-  // Right motor reverse 50, Left motor forward 50
-//    digitalWrite(rightDirectionPin, REVERSE);
-//    digitalWrite(leftDirectionPin, FORWARD);
-//    analogWrite(rightSpeedPWMPin, 50);
-//    analogWrite(leftSpeedPWMPin, 50);
-
-  // Right motor forward 100, Left motor forward 100
-//    digitalWrite(rightDirectionPin, FORWARD);
-//    digitalWrite(leftDirectionPin, FORWARD);
-//    analogWrite(rightSpeedPWMPin, 100);
-//    analogWrite(leftSpeedPWMPin, 100);
-
-  // Right motor reverse 100, Left motor reverse 100
-//    digitalWrite(rightDirectionPin, REVERSE);
-//    digitalWrite(leftDirectionPin, REVERSE);
-//    analogWrite(rightSpeedPWMPin, 100);
-//    analogWrite(leftSpeedPWMPin, 100);
-
-  // Right motor forward 0, Left motor forward 0
-    digitalWrite(rightDirectionPin, FORWARD);
-    digitalWrite(leftDirectionPin, FORWARD);
-    analogWrite(rightSpeedPWMPin, 0);
-    analogWrite(leftSpeedPWMPin, 0);
 }
 
 // LOOP ////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-  if(compassReadings <= 200){
+  if(compassReadings <= 5/*000*/ && !isCalibrationCompleted){
     compass.read();
     snprintf(compassReport, sizeof(compassReport), "%6d,%6d,%6d", compassReadings, compass.m.x, compass.m.y);
     Serial.println(compassReport);
     compassReadings++;
   }
-  delay(250);
+  else if(!isCalibrationCompleted) {
+    Serial.println();
+    Serial.println();
+    setAllStop();
+    // wait a second to signal shift in operating environment
+    delay(1000);
+    operatingEnvironment++;
+    compassReadings = 0;
+    setMotorOperatingEnvironment();
+  } 
+  else {
+    // just to prevent needless looping ;-)
+    delay(10000);
+  }
+  delay(50);
+
 }
 
+void setMotorOperatingEnvironment() {
+  if(operatingEnvironment == 0) 
+  {
+    setOperatingEnvironmentZero();
+  } 
+  else if(operatingEnvironment == 1) 
+  {
+    setOperatingEnvironmentOne();
+  } 
+  else if(operatingEnvironment == 2) 
+  {
+    setOperatingEnvironmentTwo();
+  } 
+  else if(operatingEnvironment == 3) 
+  {
+    setOperatingEnvironmentThree();
+  } 
+  else if(operatingEnvironment == 4) 
+  {
+    setOperatingEnvironmentFour();
+  } 
+  else if(operatingEnvironment == 5) {
+    isCalibrationCompleted = true;
+    setAllStop();
+    tone(buzzerDigitalPin, 500);
+    delay(500);
+    noTone(buzzerDigitalPin);
+    delay(500);
+    tone(buzzerDigitalPin, 500);
+    delay(500);
+    noTone(buzzerDigitalPin);
+    delay(500);
 
+  } 
+  Serial.println("-------------------------------------------------------");
+}  
+// OPERATING ENVIRONMENTS -- only activate one -----------------------------------------------------------------
+
+void setOperatingEnvironmentZero() {
+  Serial.println("Setting Operating Environment Zero: [Right motor forward 50, Left motor reverse 50]");
+  // Right motor forward 50, Left motor reverse 50
+  digitalWrite(rightDirectionPin, FORWARD);
+  digitalWrite(leftDirectionPin, REVERSE);
+  analogWrite(rightSpeedPWMPin, 50);
+  analogWrite(leftSpeedPWMPin, 50);
+}
+
+void setOperatingEnvironmentOne() {
+  Serial.println("Setting Operating Environment One: [Right motor reverse 50, Left motor forward 50]");
+  // Right motor reverse 50, Left motor forward 50
+  digitalWrite(rightDirectionPin, REVERSE);
+  digitalWrite(leftDirectionPin, FORWARD);
+  analogWrite(rightSpeedPWMPin, 50);
+  analogWrite(leftSpeedPWMPin, 50);
+}
+
+void setOperatingEnvironmentTwo() {
+  Serial.println("Setting Operating Environment Two: [Right motor forward 100, Left motor forward 100]");
+  // Right motor forward 100, Left motor forward 100
+  digitalWrite(rightDirectionPin, FORWARD);
+  digitalWrite(leftDirectionPin, FORWARD);
+  analogWrite(rightSpeedPWMPin, 100);
+  analogWrite(leftSpeedPWMPin, 100);
+}
+
+void setOperatingEnvironmentThree() {
+  Serial.println("Setting Operating Environment Three: [Right motor reverse 100, Left motor reverse 100]");
+  // Right motor reverse 100, Left motor reverse 100
+  digitalWrite(rightDirectionPin, REVERSE);
+  digitalWrite(leftDirectionPin, REVERSE);
+  analogWrite(rightSpeedPWMPin, 100);
+  analogWrite(leftSpeedPWMPin, 100);
+}
+
+void setOperatingEnvironmentFour() {
+  Serial.println("Setting Operating Environment Four: [Right motor forward 0, Left motor forward 0]");
+  // Right motor forward 0, Left motor forward 0
+  digitalWrite(rightDirectionPin, FORWARD);
+  digitalWrite(leftDirectionPin, FORWARD);
+  analogWrite(rightSpeedPWMPin, 0);
+  analogWrite(leftSpeedPWMPin, 0);
+}
+
+void setAllStop() {
+  Serial.println("Setting All Stop: [Right motor forward 0, Left motor forward 0]");
+  // Right motor forward 0, Left motor forward 0
+  digitalWrite(rightDirectionPin, FORWARD);
+  digitalWrite(leftDirectionPin, FORWARD);
+  analogWrite(rightSpeedPWMPin, 0);
+  analogWrite(leftSpeedPWMPin, 0);
+}
 
 
 
